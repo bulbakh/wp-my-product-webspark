3c1cddb9f291da728fe51cb35659fa74bfe774c4
bulbakh <bulbakh@gmail.com>
2024-12-15 16:44:57 +0200

    added products pagination

--- includes/class-wmpw-endpoints.php
+++ includes/class-wmpw-endpoints.php
@@ -16,6 +16,19 @@ class WMPW_Endpoints {
 	public function __invoke(): void {
 		add_rewrite_endpoint( 'add-product', EP_ROOT | EP_PAGES );
 		add_rewrite_endpoint( 'my-products', EP_ROOT | EP_PAGES );
+		$this->wmpw_add_products_paginationrewrite_rule();
+
 		flush_rewrite_rules();
+	}
+
+	/**
+	 * Add my products rewrite rule for pagination
+	 */
+	private function wmpw_add_products_paginationrewrite_rule(): void {
+		add_rewrite_rule(
+			'my-account/my-products/page/([0-9]{1,})/?$',
+			'index.php?pagename=my-account&my-products&paged=$matches[1]',
+			'top'
+		);
 	}
 }
